6cbcdabecf3f7431d451cf4f721a4e13501d28cb
algayer <matheusalgayer15@gmail.com>
2023-10-25 10:50:48 -0300

    change EquipeDAO

--- src/com/example/common/dao/EquipeDAO.java
+++ src/com/example/common/dao/EquipeDAO.java
@@ -1,6 +1,8 @@
 package com.example.common.dao;
 
 import com.example.common.model.Equipe;
+import com.example.common.model.Pessoa;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,17 +13,61 @@ import java.util.List;
 public class EquipeDAO {
 
     public void inserirEquipe(Equipe equipe) {
-        String sql = "INSERT INTO Equipe (nome) VALUES (?)";
+        String sqlEquipe = "INSERT INTO Equipe (nome) VALUES (?)";
+        String sqlPessoaEquipe = "INSERT INTO Pessoa_has_Equipe (Pessoa_ID_Pessoa, Equipe_ID_Equipe) VALUES (?, ?)";
 
-        try (Connection conexao = ConexaoBancoDados.abrirConexao();
-                PreparedStatement stmt = conexao.prepareStatement(sql)) {
-            stmt.setString(1, equipe.getNome());
-            stmt.executeUpdate();
+        Connection conexao = null;
+        PreparedStatement stmtEquipe = null;
+        PreparedStatement stmtPessoaEquipe = null;
+        ResultSet generatedKeys = null;
+
+        try {
+            conexao = ConexaoBancoDados.abrirConexao();
+            conexao.setAutoCommit(false); // Desabilita o auto-commit para controle manual de transação
+
+            // Insere a equipe
+            stmtEquipe = conexao.prepareStatement(sqlEquipe, Statement.RETURN_GENERATED_KEYS);
+            stmtEquipe.setString(1, equipe.getNome());
+            stmtEquipe.executeUpdate();
+
+            // Obtém o ID da equipe inserida
+            generatedKeys = stmtEquipe.getGeneratedKeys();
+            int idEquipe = 0;
+            if (generatedKeys.next()) {
+                idEquipe = generatedKeys.getInt(1);
+            } else {
+                throw new SQLException("Criação da equipe falhou, nenhum ID obtido.");
+            }
+
+            // Associa cada membro à equipe
+            stmtPessoaEquipe = conexao.prepareStatement(sqlPessoaEquipe);
+            for (Pessoa membro : equipe.getMembros()) {
+                stmtPessoaEquipe.setInt(1, membro.getID_Pessoa());
+                stmtPessoaEquipe.setInt(2, idEquipe);
+                stmtPessoaEquipe.executeUpdate();
+            }
+
             ConexaoBancoDados.commit(); // Realiza o commit da transação
         } catch (SQLException e) {
             ConexaoBancoDados.rollback(); // Em caso de erro, faz o rollback
             e.printStackTrace();
         } finally {
+            // Fecha as conexões e statements
+            if (generatedKeys != null) try {
+                generatedKeys.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (stmtEquipe != null) try {
+                stmtEquipe.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (stmtPessoaEquipe != null) try {
+                stmtPessoaEquipe.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             ConexaoBancoDados.fecharConexao(); // Fecha a conexão
         }
     }
@@ -29,8 +75,7 @@ public class EquipeDAO {
     public void atualizarEquipe(Equipe equipe) {
         String sql = "UPDATE Equipe SET nome=? WHERE ID_equipe=?";
 
-        try (Connection conexao = ConexaoBancoDados.abrirConexao();
-                PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try ( Connection conexao = ConexaoBancoDados.abrirConexao();  PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setString(1, equipe.getNome());
             stmt.setInt(2, equipe.getID_Equipe());
             stmt.executeUpdate();
@@ -46,8 +91,7 @@ public class EquipeDAO {
     public void excluirEquipe(int idEquipe) {
         String sql = "DELETE FROM Equipe WHERE ID_equipe=?";
 
-        try (Connection conexao = ConexaoBancoDados.abrirConexao();
-                PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try ( Connection conexao = ConexaoBancoDados.abrirConexao();  PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setInt(1, idEquipe);
             stmt.executeUpdate();
             ConexaoBancoDados.commit(); // Realiza o commit da transação
@@ -63,8 +107,7 @@ public class EquipeDAO {
         String sql = "SELECT * FROM Equipe WHERE ID_equipe=?";
         Equipe equipe = null;
 
-        try (Connection conexao = ConexaoBancoDados.abrirConexao();
-                PreparedStatement stmt = conexao.prepareStatement(sql)) {
+        try ( Connection conexao = ConexaoBancoDados.abrirConexao();  PreparedStatement stmt = conexao.prepareStatement(sql)) {
             stmt.setInt(1, idEquipe);
             ResultSet resultado = stmt.executeQuery();
 
@@ -85,9 +128,7 @@ public class EquipeDAO {
         List<Equipe> listaEquipes = new ArrayList<>();
         String sql = "SELECT * FROM Equipe";
 
-        try (Connection conexao = ConexaoBancoDados.abrirConexao();
-                PreparedStatement stmt = conexao.prepareStatement(sql);
-                ResultSet resultado = stmt.executeQuery()) {
+        try ( Connection conexao = ConexaoBancoDados.abrirConexao();  PreparedStatement stmt = conexao.prepareStatement(sql);  ResultSet resultado = stmt.executeQuery()) {
             while (resultado.next()) {
                 int idEquipe = resultado.getInt("ID_equipe");
                 String nome = resultado.getString("nome");
@@ -103,4 +144,3 @@ public class EquipeDAO {
         return listaEquipes;
     }
 }
-
--- src/com/example/common/model/Equipe.java
+++ src/com/example/common/model/Equipe.java
@@ -1,20 +1,28 @@
 package com.example.common.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Equipe implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private int ID_Equipe;
     private String Nome;
+    private List<Pessoa> membros;
 
     public Equipe() {
         // Construtor vazio é necessário para serialização
     }
-
+    
     public Equipe(int ID_Equipe, String Nome) {
         this.ID_Equipe = ID_Equipe;
         this.Nome = Nome;
+    }
+    
+    public Equipe(int ID_Equipe, String nome, List<Pessoa> membros) {
+        this.ID_Equipe = ID_Equipe;
+        this.Nome = nome;
+        this.membros = membros;
     }
 
     // Getters e Setters
@@ -33,12 +41,21 @@ public class Equipe implements Serializable {
     public void setNome(String Nome) {
         this.Nome = Nome;
     }
+    
+    public List<Pessoa> getMembros() {
+        return membros;
+    }
+
+    public void setMembros(List<Pessoa> membros) {
+        this.membros = membros;
+    }
 
     @Override
     public String toString() {
         return "Equipe{" +
                 "ID_Equipe=" + ID_Equipe +
                 ", Nome='" + Nome + '\'' +
+                ", Membros=" + membros +
                 '}';
     }
 }
